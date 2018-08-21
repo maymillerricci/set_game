0d9c1e40dc61d2837966a4c2db0d99a51fcbb8a9
May Miller-Ricci <maymillerricci@gmail.com>
2018-08-21 12:33:53 -0400

    update game test

--- test/set_game/game_test.exs
+++ test/set_game/game_test.exs
@@ -1,18 +1,27 @@
 defmodule SetGame.GameTest do
   use SetGame.ModelCase, async: true
   alias SetGame.Game
+  alias SetGame.Player
 
   setup do
-    {:ok, state} = Game.start_link()
+    {:ok, game} = Game.start_link()
 
-    {:ok, state: state}
+    {:ok, game: game}
   end
 
   describe "join" do
-    test "player numbers are sequential as they join", %{state: state} do
-      {:ok, 1} = SetGame.Game.join(state)
-      {:ok, 2} = SetGame.Game.join(state)
-      {:ok, 3} = SetGame.Game.join(state)
+    test "player numbers and counts are sequential as they join", %{game: game} do
+      assert {:ok, 1, [%Player{}]} = Game.join(game)
+      assert {:ok, 2, [%Player{}, %Player{}]} = Game.join(game)
+      assert {:ok, 3, [%Player{} , %Player{}, %Player{}]} = Game.join(game)
+    end
+  end
+
+  describe "start" do
+    test "initializes the game board", %{game: game} do
+      {:ok, board} = Game.start(game)
+
+      assert length(board) == 12
     end
   end
 end

3f9bbd0842e3351f68b10be7392b2bc6fba2531d
May Miller-Ricci <maymillerricci@gmail.com>
2018-08-21 12:50:01 -0400

    update tests

--- test/controllers/game_controller_test.exs
+++ test/controllers/game_controller_test.exs
@@ -5,4 +5,9 @@ defmodule SetGame.GameControllerTest do
     conn = get(conn, game_path(conn, :new))
     assert redirected_to(conn) =~ "/game/"
   end
+
+  test "show", %{conn: conn} do
+    conn = get(conn, game_path(conn, :show, Ecto.UUID.generate()))
+    assert html_response(conn, 200) =~ "Start Game"
+  end
 end
